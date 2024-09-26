5a78d0361edd674d7b9caa7a122f5816a7566f47
Divyanshu Patel <147162942+divyanshupatel17@users.noreply.github.com>
2024-09-26 07:35:39 +0530

    Update temp.java

--- temp.java
+++ temp.java
@@ -1,3 +1,219 @@
+import java.util.*;
+
+// Interface representing basic account operations
+interface AccountOperations {
+    void deposit(float amount); // abstract method for deposit
+    void withdraw(float amount); // abstract method for withdraw
+    void displayBalance(); // abstract method to display balance
+}
+
+// Abstract class representing a bank account
+abstract class BankAccount implements AccountOperations {
+    private final int accountNumber; // final variable - unique for every account
+    protected float balance; // protected so that subclasses can access
+    private static int accountCounter = 1000; // static variable to auto-generate account numbers
+
+    // Constructor for initializing the balance
+    public BankAccount(float initialBalance) {
+        this.balance = initialBalance;
+        this.accountNumber = ++accountCounter; // auto-increment account number
+    }
+
+    // Concrete method to get account number
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    // Abstract method to calculate interest, implemented by subclasses
+    public abstract float calculateInterest();
+
+    // Static method to show total accounts created
+    public static void showTotalAccounts() {
+        System.out.println("Total Accounts Created: " + accountCounter);
+    }
+
+    // Using the 'this' keyword to show balance information
+    public void showAccountInfo() {
+        System.out.println("Account Number: " + this.accountNumber);
+        System.out.println("Balance: " + this.balance);
+    }
+
+    // Overloaded method for withdrawing, demonstrating polymorphism (method overloading)
+    public void withdraw(float amount, boolean isATM) {
+        if (isATM) {
+            System.out.println("ATM Withdrawal processing...");
+        }
+        withdraw(amount); // calls original withdraw method
+    }
+}
+
+// Subclass representing a savings account
+class SavingsAccount extends BankAccount {
+    private final float interestRate;
+
+    // Parameterized constructor
+    public SavingsAccount(float initialBalance, float interestRate) {
+        super(initialBalance); // calling superclass constructor using 'super'
+        this.interestRate = interestRate;
+    }
+
+    // Implementing abstract method from parent class
+    @Override
+    public float calculateInterest() {
+        return balance * (interestRate / 100);
+    }
+
+    // Overriding withdraw method from interface (method overriding)
+    @Override
+    public void withdraw(float amount) {
+        if (amount > balance) {
+            System.out.println("Insufficient funds!");
+        } else {
+            balance -= amount;
+            System.out.println("Withdrawal successful! New Balance: " + balance);
+        }
+    }
+
+    // Overriding deposit method from interface
+    @Override
+    public void deposit(float amount) {
+        balance += amount;
+        System.out.println("Deposit successful! New Balance: " + balance);
+    }
+
+    @Override
+    public void displayBalance() {
+        System.out.println("Savings Account Balance: " + balance);
+    }
+}
+
+// Subclass representing a current account
+class CurrentAccount extends BankAccount {
+    private float overdraftLimit;
+
+    // Constructor
+    public CurrentAccount(float initialBalance, float overdraftLimit) {
+        super(initialBalance); // calling superclass constructor
+        this.overdraftLimit = overdraftLimit;
+    }
+
+    @Override
+    public float calculateInterest() {
+        return 0; // No interest for current accounts
+    }
+
+    @Override
+    public void withdraw(float amount) {
+        if (amount > (balance + overdraftLimit)) {
+            System.out.println("Overdraft limit exceeded!");
+        } else {
+            balance -= amount;
+            System.out.println("Withdrawal successful! New Balance: " + balance);
+        }
+    }
+
+    @Override
+    public void deposit(float amount) {
+        balance += amount;
+        System.out.println("Deposit successful! New Balance: " + balance);
+    }
+
+    @Override
+    public void displayBalance() {
+        System.out.println("Current Account Balance: " + balance);
+    }
+}
+
+// Subclass representing a fixed deposit account (final class cannot be inherited)
+final class FixedDepositAccount extends BankAccount {
+    private final float interestRate;
+    private final int depositTerm;
+
+    // Constructor
+    public FixedDepositAccount(float initialBalance, float interestRate, int depositTerm) {
+        super(initialBalance);
+        this.interestRate = interestRate;
+        this.depositTerm = depositTerm;
+    }
+
+    // Overriding abstract method from BankAccount
+    @Override
+    public float calculateInterest() {
+        return balance * (interestRate / 100) * depositTerm;
+    }
+
+    // Preventing withdrawal from Fixed Deposit
+    @Override
+    public void withdraw(float amount) {
+        System.out.println("Withdrawal not allowed from Fixed Deposit Account until maturity!");
+    }
+
+    @Override
+    public void deposit(float amount) {
+        System.out.println("Deposit not allowed in Fixed Deposit Account after initial deposit.");
+    }
+
+    @Override
+    public void displayBalance() {
+        System.out.println("Fixed Deposit Account Balance: " + balance);
+    }
+}
+
+// Class representing the bank with an array of objects (customers)
+class Bank {
+    private final List<BankAccount> accounts = new ArrayList<>();
+
+    // Method to add accounts to the bank
+    public void addAccount(BankAccount account) {
+        accounts.add(account);
+    }
+
+    // Method to display account information for all customers
+    public void displayAllAccounts() {
+        for (BankAccount account : accounts) {
+            account.showAccountInfo();
+            account.displayBalance();
+            System.out.println("Interest: " + account.calculateInterest());
+            System.out.println("--------------------------");
+        }
+    }
+}
+
+// Main class to simulate the banking system
+public class BankingSystem {
+    public static void main(String[] args) {
+        // Array of objects
+        Bank bank = new Bank();
+
+        // Creating accounts using parameterized constructors
+        SavingsAccount savings1 = new SavingsAccount(1000, 3.5f);
+        CurrentAccount current1 = new CurrentAccount(2000, 500);
+        FixedDepositAccount fd1 = new FixedDepositAccount(5000, 5.5f, 2);
+
+        // Adding accounts to the bank
+        bank.addAccount(savings1);
+        bank.addAccount(current1);
+        bank.addAccount(fd1);
+
+        // Showing account information and balances
+        bank.displayAllAccounts();
+
+        // Static method call
+        BankAccount.showTotalAccounts();
+
+        // Demonstrating garbage collection
+        fd1 = null;
+        System.gc(); // Requesting garbage collection
+    }
+
+    // Override finalize method to see when garbage collection happens
+    @Override
+    protected void finalize() throws Throwable {
+        System.out.println("Garbage collection performed!");
+    }
+}
+
+
 class Book {
     private String title;
     private String author;
